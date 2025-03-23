18259f491844efe428f0cc7dcfe7643c575c5c7b
Kende Schultheisz <kyokende@gmail.com>
2025-03-23 12:28:31 +0100

    Setup events for score updating

---
+++ Assets/Scripts/Events/EventChannel.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EventChannel", menuName = "Scriptable Objects/EventChannel")]
+public class EventChannel : ScriptableObject
+{
+
+    // This event should be called, whenever the score changes -- called by the individual gameObjects
+    public event Action<int> OnScoreChanged;
+    // This event should be called, whenever the hud should be updated -- called by the ScoreManager
+    public event Action<int> OnUpdateHud;
+
+    /// <param name="score">The value that should be added to the current scores of the player</param>
+    public void RaiseScoreChanged(int score)
+    {
+        OnScoreChanged?.Invoke(score);
+    }
+
+    public void RaiseUpdateHud(int currentScore) {
+        OnUpdateHud?.Invoke(currentScore);
+    }    
+
+
+}
